7085cccbdf9e8588cfef68f74a3888419281f823
24209 <2420975408@qq.com>
2026-09-07 23:30:52 +0800

    跨模态识别

--- agent-scaffold-domain/src/main/java/daoha/top/domain/agent/service/armory/matter/plugin/MyTestPlugin.java
+++ agent-scaffold-domain/src/main/java/daoha/top/domain/agent/service/armory/matter/plugin/MyTestPlugin.java
@@ -49,10 +49,4 @@ public class MyTestPlugin extends BasePlugin {
         return super.beforeAgentCallback(agent, callbackContext);
     }
 
-    @Override
-    public Maybe<LlmResponse> beforeModelCallback(CallbackContext callbackContext, LlmRequest llmRequest) {
-        Optional<String> model = llmRequest.model();
-        log.info("ai模型:{}",model.orElse(""));
-        return super.beforeModelCallback(callbackContext, llmRequest);
-    }
 }
